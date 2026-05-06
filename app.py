50a935dd44c2947b5c687bf90ee7b44ad9954263
RoqaiaKhalaf <roqaiamohamed0707@gmail.com>
2026-05-07 01:29:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ with st.container():
     col1, col2, col3 = st.columns([1, 1, 1])
     with col2:
         try:
-            st.image("logo.png", width=140)
+            st.image("logo.png", width=100)
         except:
             st.write("<div style='text-align:center; font-size:3rem;'>🔬</div>", unsafe_allow_html=True)
 
